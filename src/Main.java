073692254ac74fd750e704815060003983827dfe
Yonatan Leulseged <leulsegedyonatan@gmail.com>
2024-07-17 12:49:15 +0300

    added different methods.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
-import arrays.Array;
 import linkedlists.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
        LinkedList list = new LinkedList();
-       list.addFirst(9);
+//       list.addFirst(9);
 //       list.addFirst(10);
 //       list.addFirst(11);
-//       list.addFirst(12);
 
 
-       list.addLast(50);
-       list.addLast(51);
+//     list.addFirst(11);
+//     list.addFirst(12);
+
+
+//       list.addLast(50);
+//       list.addLast(51);
+        System.out.println(list.indexOf(50));
+        list.removeFirst();
+        System.out.println(list.contains(10));
+
+
+       //start from linked list ep07
 
 
 
--- src/linkedlists/LinkedList.java
+++ src/linkedlists/LinkedList.java
@@ -1,6 +1,7 @@
 package linkedlists;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class LinkedList {
     private class Node {
@@ -16,21 +17,59 @@ public class LinkedList {
     private Node first;
     private Node last;
 
-    private ArrayList<Node> output = new ArrayList<>();
+    private final ArrayList<Node> nodes = new ArrayList<>();
 
 
     public void addFirst(int item){
-        if(first == null)
+        if(first == null) {
             last = first = new Node(item, null);
-        else
+            nodes.addFirst(first);
+        }else {
             first = new Node(item, first);
+            nodes.addFirst(first);
+        }
     }
 
     public void addLast(int item){
-//        if(first == null)
-//            first = last = new Node(item, null);
-//        else
             last = new Node(item, last);
+            nodes.addLast(last);
+    }
+
+    int index = 0;
+    public int indexOf(int item){
+//        for(Node node : nodes){
+//            if (node.value == item)
+//                return index;
+//            index++;
+//        }
+
+        var current = last;
+        while (current != null){
+            if(current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item){
+        var current = last;
+        while (current != null){
+            if(current.value == item) return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    public void removeFirst(){
+
+
+        if( first != null)  {
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }else throw new NoSuchElementException();
+
 
     }
 
